f0c2d21312f5850be6ce51d38a827c0132985320
hmztplinv <hmz.tpal@gmail.com>
2024-05-09 16:17:58 +0300

    finalize

--- wm.py
+++ wm.py
@@ -1,8 +1,10 @@
 from PyPDF2 import PdfReader, PdfWriter
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.lib.colors import red
 import io
 import os
+from datetime import datetime
 
 # Çalışma dizinini yazdır
 print("Çalışma Dizini:", os.getcwd())
@@ -18,7 +20,9 @@ def check_file_existence(file_path):
 def create_stamp():
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(10, 100, "Bu belge Canias ERP tarafından onaylanmıştır.")
+    can.setFont("Helvetica-Bold", 12)  # Kalın font ayarı
+    can.setFillColor(red)  # Kırmızı renk ayarı
+    can.drawString(10, 20, "Bu belge Canias ERP tarafindan onaylanmistir.")  # Sayfanın altına yaz
     can.save()
     packet.seek(0)
     return packet
@@ -41,9 +45,20 @@ def add_stamp_to_pdf(input_pdf, output_pdf, stamp_stream):
     with open(output_pdf, "wb") as f_out:
         writer.write(f_out)
 
+# Klasördeki tüm PDF'leri işle
+def process_all_pdfs(source_dir, destination_dir):
+    current_time = datetime.now().strftime("%d.%m.%Y %H.%M.%S")
+    for filename in os.listdir(source_dir):
+        if filename.endswith(".pdf"):
+            input_pdf_path = os.path.join(source_dir, filename)
+            new_filename = filename.replace(".pdf", f" {current_time}_damgali.pdf")
+            output_pdf_path = os.path.join(destination_dir, new_filename)
+            stamp = create_stamp()
+            add_stamp_to_pdf(input_pdf_path, output_pdf_path, stamp)
+
 # Ana işlem
-input_pdf_path = "Source/1.pdf"  # Girdi olarak kullanılacak PDF dosyası yolu
-output_pdf_path = "Destination/n1.pdf"  # Çıktı olarak kaydedilecek damgalı PDF dosyası yolu
-stamp = create_stamp()
-add_stamp_to_pdf(input_pdf_path, output_pdf_path, stamp)
+source_directory = "Source"  # Kaynak klasör yolu
+destination_directory = "Destination"  # Hedef klasör yolu
+process_all_pdfs(source_directory, destination_directory)
+
 
